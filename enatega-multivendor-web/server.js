4d6923aefe428e4baca5de2eca2ac0ff177de219
ahmedelselly <ahmedelselly87@gmail.com>
2025-01-02 16:43:21 +0200

    changed the server port of customer web

--- enatega-multivendor-web/server.js
+++ enatega-multivendor-web/server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3001;
 
 // Serve static files from the build folder
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, "build")));
 
 // Serve the React app for any other route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`React app is running on port ${port}`)
-})
+  console.log(`React app is running on port ${port}`);
+});
