189087a030f9b4d65b98f505970e1cee79359aa7
ahmedelselly <ahmedelselly87@gmail.com>
2025-01-02 17:07:31 +0200

    changed the port

--- enatega-multivendor-web/server.js
+++ enatega-multivendor-web/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 
 // Serve static files from the build folder
 app.use(express.static(path.join(__dirname, "build")));
