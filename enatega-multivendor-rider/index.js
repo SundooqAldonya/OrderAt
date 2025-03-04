e4544c2c9e797abd0c7201ba143c3db81ef69b21
Ahmed Elselly <ahmedelselly87@gmail.com>
2025-03-04 10:27:37 +0200

    added condition for background notification sound - not working

--- enatega-multivendor-rider/index.js
+++ enatega-multivendor-rider/index.js
@@ -1,8 +1,14 @@
 import 'expo-dev-client'
-import { registerRootComponent } from 'expo'
-
 import App from './App'
+import { registerRootComponent } from 'expo'
+import messaging from '@react-native-firebase/messaging'
+import { playCustomSound } from './src/utilities/playSound'
 
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  if (remoteMessage.notification.android.sound !== 'false') {
+    await playCustomSound()
+  }
+})
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
