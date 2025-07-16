d40e081530b27bb056817dfe923547b73ca5b473
Ajay Nagwani <aj.nagwani69.com>
2025-07-16 22:07:22 +0530

    Fixed otlob crash and network timeout issue

--- enatega-multivendor-app/src/apollo/index.js
+++ enatega-multivendor-app/src/apollo/index.js
@@ -17,6 +17,8 @@ import useEnvVars from '../../environment'
 import { useContext } from 'react'
 import { LocationContext } from '../context/Location'
 import { calculateDistance } from '../utils/customFunctions'
+import { RetryLink } from '@apollo/client/link/retry';
+import { onError } from '@apollo/client/link/error';
 
 const setupApollo = () => {
   const { GRAPHQL_URL, WS_GRAPHQL_URL } = useEnvVars()
@@ -91,6 +93,33 @@ const setupApollo = () => {
     }
   })
 
+  const retryLink = new RetryLink({
+  attempts: {
+    max: 2,
+    retryIf: async (error, operation) => {
+      console.log('retryLink error-->>', error);
+      if (!error) {
+        return false;
+      }
+      // Attempt to refresh the token if it's an authentication error
+      return true;
+    },
+  },
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  }
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
   const request = async (operation) => {
     // await AsyncStorage.removeItem('token')
     const token = await AsyncStorage.getItem('token')
@@ -140,7 +169,7 @@ const setupApollo = () => {
   )
 
   const client = new ApolloClient({
-    link: ApolloLink.from([terminatingLink, requestLink, httpLink]),
+    link: ApolloLink.from([retryLink, errorLink, terminatingLink, requestLink, httpLink]),
     cache,
     resolvers: {}
   })
