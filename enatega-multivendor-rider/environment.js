fa37d5e9d435c36eb366d75d6b56e33141f1bc2b
Ahmed Elselly <ahmedelselly87@gmail.com>
2025-07-27 10:32:45 +0300

    added keyboard avoiding view in login screen - rider app

--- enatega-multivendor-rider/environment.js
+++ enatega-multivendor-rider/environment.js
@@ -8,21 +8,21 @@ const getEnvVars = (env = Updates.channel) => {
   console.log('configuration', configuration)
   console.log('node_env', env)
 
-  // if (env && (env === 'production' || env === 'staging')) {
+  if (env && (env === 'production' || env === 'staging')) {
+    return {
+      GRAPHQL_URL: 'https://query.orderat.ai/graphql',
+      WS_GRAPHQL_URL: 'wss://query.orderat.ai/graphql',
+      SENTRY_DSN: configuration.riderAppSentryUrl,
+      GOOGLE_MAPS_KEY: configuration.googleApiKey
+    }
+  }
   return {
-    GRAPHQL_URL: 'https://query.orderat.ai/graphql',
-    WS_GRAPHQL_URL: 'wss://query.orderat.ai/graphql',
+    GRAPHQL_URL: 'http://192.168.1.8:8001/graphql',
+    WS_GRAPHQL_URL: 'ws://192.168.1.8:8001/graphql',
+    SERVER_URL: 'http://192.168.1.8:8001/',
     SENTRY_DSN: configuration.riderAppSentryUrl,
     GOOGLE_MAPS_KEY: configuration.googleApiKey
   }
-  // }
-  // return {
-  //   GRAPHQL_URL: 'http://192.168.1.8:8001/graphql',
-  //   WS_GRAPHQL_URL: 'ws://192.168.1.8:8001/graphql',
-  //   SERVER_URL: 'http://192.168.1.8:8001/',
-  //   SENTRY_DSN: configuration.riderAppSentryUrl,
-  //   GOOGLE_MAPS_KEY: configuration.googleApiKey
-  // }
 }
 
 export default getEnvVars
