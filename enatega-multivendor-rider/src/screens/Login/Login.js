45150dd2ddce51bc2dcd0a13589eb2e72b77069b
Ahmed Elselly <ahmedelselly87@gmail.com>
2025-07-27 10:32:30 +0300

    added keyboard avoiding view in login screen - rider app

--- enatega-multivendor-rider/src/screens/Login/Login.js
+++ enatega-multivendor-rider/src/screens/Login/Login.js
@@ -4,7 +4,11 @@ import {
   TouchableOpacity,
   TextInput,
   Image,
-  ScrollView
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import styles from './styles'
@@ -40,74 +44,82 @@ export default function Login() {
 
   return (
     <SafeAreaView style={[styles.flex, styles.bgColor]}>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ height: height * 1 }}
-        style={styles.container}>
-        <Image
-          source={RiderLogin}
-          style={[styles.image]}
-          height={150}
-          width={250}
-        />
-        <View style={styles.innerContainer}>
-          <TextDefault bolder H2 center style={styles.signInText}>
-            {t('signInText')}
-          </TextDefault>
-          <TextInput
-            style={[styles.textInput, usernameError && styles.errorInput]}
-            placeholder={t('username')}
-            value={username}
-            onChangeText={e => setUsername(e)}
-          />
-          {usernameError ? (
-            <TextDefault
-              style={styles.error}
-              bold
-              textColor={colors.textErrorColor}>
-              {usernameError}
-            </TextDefault>
-          ) : null}
-          <View style={styles.passwordField}>
-            <TextInput
-              secureTextEntry={showPassword}
-              placeholder={t('password')}
-              style={[
-                styles.textInput,
-                styles.passwordInput,
-                passwordError && styles.errorInput
-              ]}
-              value={password}
-              onChangeText={e => setPassword(e)}
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0}>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            // contentContainerStyle={{ height: height * 1 }}
+            contentContainerStyle={{ flexGrow: 1 }}
+            style={styles.container}>
+            <Image
+              source={RiderLogin}
+              style={[styles.image]}
+              height={150}
+              width={250}
             />
-            <FontAwesome
-              onPress={() => setShowPassword(!showPassword)}
-              name={showPassword ? 'eye' : 'eye-slash'}
-              size={24}
-              style={styles.eyeBtn}
-            />
-            {console.log(username)}
-          </View>
-          {passwordError ? (
-            <View>
-              <TextDefault
-                style={styles.error}
-                bold
-                textColor={colors.textErrorColor}>
-                {passwordError}
+            <View style={styles.innerContainer}>
+              <TextDefault bolder H2 center style={styles.signInText}>
+                {t('signInText')}
               </TextDefault>
+              <TextInput
+                style={[styles.textInput, usernameError && styles.errorInput]}
+                placeholder={t('username')}
+                value={username}
+                onChangeText={e => setUsername(e)}
+              />
+              {usernameError ? (
+                <TextDefault
+                  style={styles.error}
+                  bold
+                  textColor={colors.textErrorColor}>
+                  {usernameError}
+                </TextDefault>
+              ) : null}
+              <View style={styles.passwordField}>
+                <TextInput
+                  secureTextEntry={showPassword}
+                  placeholder={t('password')}
+                  style={[
+                    styles.textInput,
+                    styles.passwordInput,
+                    passwordError && styles.errorInput
+                  ]}
+                  value={password}
+                  onChangeText={e => setPassword(e)}
+                />
+                <FontAwesome
+                  onPress={() => setShowPassword(!showPassword)}
+                  name={showPassword ? 'eye' : 'eye-slash'}
+                  size={24}
+                  style={styles.eyeBtn}
+                />
+                {console.log(username)}
+              </View>
+              {passwordError ? (
+                <View>
+                  <TextDefault
+                    style={styles.error}
+                    bold
+                    textColor={colors.textErrorColor}>
+                    {passwordError}
+                  </TextDefault>
+                </View>
+              ) : null}
+              <TouchableOpacity
+                activeOpacity={0.7}
+                style={[styles.btn, loading ? styles.pt5 : styles.pt15]}
+                onPress={() => onSubmit()}>
+                <TextDefault H4 bold textColor={colors.white}>
+                  {loading ? <Spinner size="small" /> : t('signInBtn')}
+                </TextDefault>
+              </TouchableOpacity>
             </View>
-          ) : null}
-          <TouchableOpacity
-            activeOpacity={0.7}
-            style={[styles.btn, loading ? styles.pt5 : styles.pt15]}
-            onPress={() => onSubmit()}>
-            <TextDefault H4 bold textColor={colors.white}>
-              {loading ? <Spinner size="small" /> : t('signInBtn')}
-            </TextDefault>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   )
 }
