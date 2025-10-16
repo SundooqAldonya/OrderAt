d2ed6dc7d5686e8ae700d8df9625ac38ddb68dd2
Ahmed Elselly <ahmedelselly87@gmail.com>
2025-10-16 13:45:55 +0300

    made admin vite project

--- enatega-multivendor-admin/src/index.js
+++ enatega-multivendor-admin/src/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+// import ReactDOM from 'react-dom'
 import {
   ApolloProvider,
   ApolloClient,
@@ -128,9 +128,15 @@ function Main() {
 }
 
 // eslint-disable-next-line react/no-deprecated
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <Main />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Main />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
