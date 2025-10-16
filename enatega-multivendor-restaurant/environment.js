dabf184c4557ac1cf407b7c5422d3dfb19fa6a23
Ahmed Elselly <ahmedelselly87@gmail.com>
2025-10-16 12:40:31 +0300

    changed the env of restaurant app

--- enatega-multivendor-restaurant/environment.js
+++ enatega-multivendor-restaurant/environment.js
@@ -1,32 +1,33 @@
-/*****************************
- * environment.js
- * path: '/environment.js' (root of your project)
- ******************************/
-
 import * as Updates from 'expo-updates'
 import { useContext } from 'react'
 import Configuration from './src/ui/context/configuration'
 
-const getEnvVars = (env = Updates.channel) => {
+const channel = Updates.channel || process.env.API_ENV || 'staging'
+
+const getEnvVars = () => {
+  console.log('Active channel:', channel)
+  console.log('Updates.channel:', Updates?.channel)
+  console.log('process.env.API_ENV:', process.env.API_ENV)
   const configuration = useContext(Configuration.Context)
 
-  console.log('configuration', configuration)
-  console.log({ env: process.env.NODE_ENV })
-
-  if (
-    process.env.NODE_ENV === 'production' ||
-    process.env.NODE_ENV === 'staging'
-  ) {
+  if (channel === 'production') {
     return {
-      // GRAPHQL_URL: 'https://query.orderat.ai/graphql',
-      // WS_GRAPHQL_URL: 'wss://query.orderat.ai/graphql',
-      // SERVER_URL: 'https://query.orderat.ai/',
       GRAPHQL_URL: 'https://service.orderatco.com/graphql',
       WS_GRAPHQL_URL: 'wss://service.orderatco.com/graphql',
       SERVER_URL: 'https://service.orderatco.com/',
       SENTRY_DSN: configuration.restaurantAppSentryUrl
     }
   }
+
+  if (channel === 'staging') {
+    return {
+      GRAPHQL_URL: 'https://querytest.orderat.ai/graphql',
+      WS_GRAPHQL_URL: 'wss://querytest.orderat.ai/graphql',
+      SERVER_URL: 'https://querytest.orderat.ai/',
+      SENTRY_DSN: configuration.restaurantAppSentryUrl
+    }
+  }
+
   return {
     GRAPHQL_URL: 'http://192.168.1.4:8001/graphql',
     WS_GRAPHQL_URL: 'ws://192.168.1.4:8001/graphql',
@@ -36,3 +37,44 @@ const getEnvVars = (env = Updates.channel) => {
 }
 
 export default getEnvVars
+
+// /*****************************
+//  * environment.js
+//  * path: '/environment.js' (root of your project)
+//  ******************************/
+
+// import * as Updates from 'expo-updates'
+// import { useContext } from 'react'
+// import Configuration from './src/ui/context/configuration'
+
+// const getEnvVars = (env = Updates.channel) => {
+//   const configuration = useContext(Configuration.Context)
+
+//   console.log('configuration', configuration)
+//   console.log({ env: process.env.NODE_ENV })
+
+//   if (process.env.NODE_ENV === 'production') {
+//     return {
+//       GRAPHQL_URL: 'https://service.orderatco.com/graphql',
+//       WS_GRAPHQL_URL: 'wss://service.orderatco.com/graphql',
+//       SERVER_URL: 'https://service.orderatco.com/',
+//       SENTRY_DSN: configuration.restaurantAppSentryUrl
+//     }
+//   }
+//   if (process.env.NODE_ENV === 'staging') {
+//     return {
+//       GRAPHQL_URL: 'https://querytest.orderat.ai/graphql',
+//       WS_GRAPHQL_URL: 'wss://querytest.orderat.ai/graphql',
+//       SERVER_URL: 'https://querytest.orderat.ai/',
+//       SENTRY_DSN: configuration.restaurantAppSentryUrl
+//     }
+//   }
+//   return {
+//     GRAPHQL_URL: 'http://192.168.1.4:8001/graphql',
+//     WS_GRAPHQL_URL: 'ws://192.168.1.4:8001/graphql',
+//     SERVER_URL: 'http://192.168.1.4:8001/',
+//     SENTRY_DSN: configuration.restaurantAppSentryUrl
+//   }
+// }
+
+// export default getEnvVars
